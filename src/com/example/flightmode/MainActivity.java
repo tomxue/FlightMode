10e9a0cb5a3462156fea816239a87ec264f4b90d
Tom Xue <tomxue0126@gmail.com>
2013-09-04 23:54:42 +0800

    add WLAN/GPRS toggle function

--- src/com/example/flightmode/MainActivity.java
+++ src/com/example/flightmode/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.flightmode;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.app.Activity;
@@ -13,9 +19,10 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 public class MainActivity extends Activity implements
 		CompoundButton.OnCheckedChangeListener {
 
-	CheckBox cb;
+	CheckBox cb1, cb2;
 	Context mContext = this;
-	String FlightModeOn, FlightModeOff;
+	String FlightModeOn, FlightModeOff, ToggleWlanGPRSOn, ToggleWlanGPRSOff;
+	WifiManager wifiManager;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -24,18 +31,33 @@ public class MainActivity extends Activity implements
 
 		FlightModeOn = getResources().getString(R.string.FlightModeOn);
 		FlightModeOff = getResources().getString(R.string.FlightModeOff);
-		cb = (CheckBox) findViewById(R.id.checkBox1);
+		ToggleWlanGPRSOn = getResources().getString(R.string.ToggleWlanGPRSOn);
+		ToggleWlanGPRSOff = getResources().getString(R.string.ToggleWlanGPRSOff);
 
-		cb.setOnCheckedChangeListener((OnCheckedChangeListener) this);
+		cb1 = (CheckBox) findViewById(R.id.checkBox1);
+		cb2 = (CheckBox) findViewById(R.id.checkBox2);
+
+		cb1.setOnCheckedChangeListener((OnCheckedChangeListener) this);
+		cb2.setOnCheckedChangeListener((OnCheckedChangeListener) this);
 	}
 
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		if (isChecked) {
-			cb.setText(FlightModeOn);
-			setAirplaneModeOn(true);
-		} else {
-			cb.setText(FlightModeOff);
-			setAirplaneModeOn(false);
+		if (buttonView.findViewById(R.id.checkBox1) == cb1) {
+			if (isChecked) {
+				cb1.setText(FlightModeOn);
+				setAirplaneModeOn(true);
+			} else {
+				cb1.setText(FlightModeOff);
+				setAirplaneModeOn(false);
+			}
+		} else if (buttonView.findViewById(R.id.checkBox2) == cb2) {
+			if (isChecked) {
+				cb2.setText(ToggleWlanGPRSOn);
+				setWlanGPRSModeOn(true);
+			} else {
+				cb2.setText(ToggleWlanGPRSOff);
+				setWlanGPRSModeOn(false);
+			}
 		}
 	}
 
@@ -54,5 +76,60 @@ public class MainActivity extends Activity implements
 		Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
 		intent.putExtra("state", enabling);
 		mContext.sendBroadcast(intent);
+	}
+	
+	private void setWlanGPRSModeOn(boolean enabled) {
+		// Change the system setting
+//		Settings.System.putInt(mContext.getContentResolver(),
+//				Settings.System.AIRPLANE_MODE_ON, enabling ? 1 : 0);
+//
+//		// Post the intent
+//		Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+//		intent.putExtra("state", enabling);
+//		mContext.sendBroadcast(intent);
+		
+		wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+		wifiManager.setWifiEnabled(enabled);
+		
+		ConnectivityManager conMgr = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+		  Class<?> conMgrClass = null; // ConnectivityManager类
+		  Field iConMgrField = null; // ConnectivityManager类中的字段
+		  Object iConMgr = null; // IConnectivityManager类的引用
+		  Class<?> iConMgrClass = null; // IConnectivityManager类
+		  Method setMobileDataEnabledMethod = null; // setMobileDataEnabled方法
+
+		  try {
+		   // 取得ConnectivityManager类
+		   conMgrClass = Class.forName(conMgr.getClass().getName());
+		   // 取得ConnectivityManager类中的对象mService
+		   iConMgrField = conMgrClass.getDeclaredField("mService");
+		   // 设置mService可访问
+		   iConMgrField.setAccessible(true);
+		   // 取得mService的实例化类IConnectivityManager
+		   iConMgr = iConMgrField.get(conMgr);
+		   // 取得IConnectivityManager类
+		   iConMgrClass = Class.forName(iConMgr.getClass().getName());
+		   // 取得IConnectivityManager类中的setMobileDataEnabled(boolean)方法
+		   setMobileDataEnabledMethod = iConMgrClass.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
+		   // 设置setMobileDataEnabled方法可访问
+		   setMobileDataEnabledMethod.setAccessible(true);
+		   // 调用setMobileDataEnabled方法
+		   setMobileDataEnabledMethod.invoke(iConMgr, !enabled);
+		  } catch (ClassNotFoundException e) {
+		   e.printStackTrace();
+		  } catch (NoSuchFieldException e) {
+		   e.printStackTrace();
+		  } catch (SecurityException e) {
+		   e.printStackTrace();
+		  } catch (NoSuchMethodException e) {
+		   e.printStackTrace();
+		  } catch (IllegalArgumentException e) {
+		   e.printStackTrace();
+		  } catch (IllegalAccessException e) {
+		   e.printStackTrace();
+		  } catch (InvocationTargetException e) {
+		   e.printStackTrace();
+		  }
 	}
 }
